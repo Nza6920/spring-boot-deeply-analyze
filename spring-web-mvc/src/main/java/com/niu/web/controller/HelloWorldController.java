46f5375afa4c5188b213bf165d123114a1582d59
Nza6920 <1484663282@qq.com>
2020-01-13 17:32:29 +0800

    spring mvc 自动注入分析

---
+++ spring-web-mvc/src/main/java/com/niu/web/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.niu.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Description: HelloWorldController
+ * @Author nza
+ * @Date 2020/1/13
+ **/
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("")
+    public String index() {
+        return "index";
+    }
+}

1d4be4b6ca6c4b75ad89744a30f7101eb6f531a0
Nza6920 <1484663282@qq.com>
2020-01-17 12:29:46 +0800

    spring framework 时代的 web mvc

--- spring-web-mvc/src/main/java/com/niu/web/controller/HelloWorldController.java
+++ spring-web-mvc/src/main/java/com/niu/web/controller/HelloWorldController.java
@@ -1,8 +1,8 @@
 package com.niu.web.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @Description: HelloWorldController
